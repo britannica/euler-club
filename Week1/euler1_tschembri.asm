72010088aaa85c8e2d3480123b012e0aa231d4a1
ebtschembri <41963369+ebtschembri@users.noreply.github.com>
2018-07-31 10:43:37 -0500

    formatting

--- Week1/euler1_tschembri.asm
+++ Week1/euler1_tschembri.asm
@@ -8,13 +8,13 @@ LOOP:
 	DIVU	#3,D2		* Divide D2 by 3: D2 lower 16 bits = result of int division, upper 16 bits = modulo
 	SWAP	D2		* Switch upper 16 bits with lower 16 bits
 	TST.W   D2		* Compare lower 16 bits with 0
-    BEQ 	SUM		* If zero, means modulo = 0 means multiple of 3, so adds the loop index to the sum
+    	BEQ 	SUM		* If zero, means modulo = 0 means multiple of 3, so adds the loop index to the sum
 	MOVE.L	D0,D2		* Same thing with 5
 	DIVU	#5,D2
 	SWAP	D2
 	TST.W   D2
 	BNE	CONTINUE
-SUM	ADD.L	D0,D1
+SUM:	ADD.L	D0,D1
 CONTINUE:
 	DBF	D0, LOOP	* Loop until D0 >= 0
 	
