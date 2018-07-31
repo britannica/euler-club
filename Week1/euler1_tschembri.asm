468c57a8d0b466f5f5183fa0f8d8b06be5e56752
ebtschembri <41963369+ebtschembri@users.noreply.github.com>
2018-07-31 10:42:20 -0500

    Update euler1_tschembri.asm

--- Week1/euler1_tschembri.asm
+++ Week1/euler1_tschembri.asm
@@ -6,9 +6,9 @@
 LOOP:
 	MOVE.L	D0,D2		* D2 = D1
 	DIVU	#3,D2		* Divide D2 by 3: D2 lower 16 bits = result of int division, upper 16 bits = modulo
-	SWAP	D2			* Switch upper 16 bits with lower 16 bits
-	TST.W   D2			* Compare lower 16 bits with 0
-    BEQ 	SUM			* If zero, means modulo = 0 means multiple of 3, so adds the loop index to the sum
+	SWAP	D2		* Switch upper 16 bits with lower 16 bits
+	TST.W   D2		* Compare lower 16 bits with 0
+    BEQ 	SUM		* If zero, means modulo = 0 means multiple of 3, so adds the loop index to the sum
 	MOVE.L	D0,D2		* Same thing with 5
 	DIVU	#5,D2
 	SWAP	D2
