f0405697631e917a4273dff022e809b0a9863607
Armin Broubakarian <ABroubakarian@eb.com>
2018-07-31 10:08:23 -0500

    less loops, same results..

--- Week1/euler1_arminbro.js
+++ Week1/euler1_arminbro.js
@@ -1,10 +1,7 @@
-const arrayofMultiples = [];
 let sum = 0;
 
 for (let i = 0; i < 1000; i++) {
-  (i % 3 === 0 || i % 5 === 0) && arrayofMultiples.push(i);
+  sum = (i % 3 === 0 || i % 5 === 0) ? sum + i : sum;
 }
-
-sum = arrayofMultiples.reduce((sum, val) => sum + val, sum);
 
 console.log(sum);
