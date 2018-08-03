bd65b1dd6f97f415644ecac98ffa1a1c98588b1e
Ryan Walters <rwalters@eb.com>
2018-08-02 21:38:41 -0500

    Update euler1_rwalters.js

--- Week1/euler1_rwalters.js
+++ Week1/euler1_rwalters.js
@@ -1,5 +1,5 @@
-function sumOfMultiples(max, divisibles) {
-  const array = Array.from(Array(max - 1)).map((v, i) => ++i).filter(v => divisibles.some(d => v % d === 0));
+function sumOfMultiples(max, multiples) {
+  const array = Array.from(Array(max - 1)).map((v, i) => ++i).filter(v => multiples.some(d => v % d === 0));
   return array.reduce((sum, val) => sum + val);
 }
 
