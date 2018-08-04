5d68c37c5f1de1edede42a71ce6f49f0fa725ddd
Mark Wiechec <mbwiechec@hotmail.com>
2018-08-04 00:25:13 -0500

    Create euler1-mwiechec-01.py

---
+++ Week1/euler1-mwiechec-01.py
@@ -0,0 +1,17 @@
+def calculate():
+  sum = 0
+  x = 3
+  while (x < 1000):
+    sum += x
+    x+=3
+  y = 5
+  while y <= 1000:
+    sum += y
+    y+=5
+    if y >= 1000: break
+    sum += y
+    y+=10
+  print (sum)
+  return 
+
+calculate()
