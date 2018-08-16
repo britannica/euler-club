0e8236500982f9581996a07e902815daff9417cb
Shawn McCullars <smccullars@eb.com>
2018-08-16 14:54:40 -0500

    more functions and a realloc (instead of creating a new array and manually doing the copy)

--- Week3/euler3_smccullars.c
+++ Week3/euler3_smccullars.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+long target_number = 600851475143;
+
 long* primes;
-long number_of_primes = 0;
+long number_of_prime_factors = 0;
 
 int push_prime(long new_prime) {
-    long old_number_of_primes = number_of_primes;
-    number_of_primes++;
-    long* new_primes = malloc(number_of_primes * sizeof(*primes));
-    for(long i=0; i < old_number_of_primes; i++) {
-        new_primes[i] = primes[i];
-    }
-    new_primes[old_number_of_primes] = new_prime;
-    primes = new_primes;
-    free(primes);
+    primes = realloc(primes, sizeof *primes);
+    primes[number_of_prime_factors] = new_prime;
+    number_of_prime_factors++;
     return 1;
 }
 
@@ -21,28 +17,33 @@ int is_factor(long x, long y) {
     return x % y == 0;
 }
 
-int is_prime(long num) {
-    for(long i=0; i < number_of_primes; i++) {
-        if(is_factor(num, primes[i])) {
+int is_new_prime_factor(long num) {
+    for(long i=0; i < number_of_prime_factors; i++) {
+        if(num == primes[i] || is_factor(num, primes[i])) {
             return 0;
         }
     }
     return 1;
 }
 
+int is_factorization_complete() {
+    long current_factor_product = 1;
+    for(int i=0; i < number_of_prime_factors; i++) {
+        current_factor_product *= primes[i];
+    }
+    if(current_factor_product == target_number) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
     primes = malloc(0);
-    long target_number = 600851475143;
     for (long i=2; i < target_number; i++) {
-        if(is_factor(target_number, i) && is_prime(i)) {
+        if(is_factor(target_number, i) && is_new_prime_factor(i)) {
             push_prime(i);
-            printf("%ld is a prime factor\n", i);
-            long current_factor_product = 1;
-            for(int j=0; j < number_of_primes; j++) {
-                current_factor_product *= primes[j];
-            }
-            if(current_factor_product == target_number) {
-                printf("largest prime: %ld\n", primes[number_of_primes-1]);
+            if(is_factorization_complete()) {
+                printf("largest prime: %ld\n", primes[number_of_prime_factors-1]);
                 return 1;
             }
         }
