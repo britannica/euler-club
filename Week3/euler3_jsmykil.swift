8cc33d47f4ed0abed542a5f939229b4fcc25a25a
Jeffrey Smykil <jeff.smykil@gmail.com>
2018-08-18 09:17:07 -0500

    Small optimization

--- Week3/euler3_jsmykil.swift
+++ Week3/euler3_jsmykil.swift
@@ -12,15 +12,15 @@ let goal : Double = 600_851_475_143.0;
 var root : Double = goal.squareRoot()
 	root.round()
 
-for x in stride(from: 2, through: root, by: 1) {
+for x in stride(from: root, through: 2, by: -1) {
 	let onesPlace = x.truncatingRemainder(dividingBy: 10);
 	if (onesPlace == 1 || onesPlace == 3 || onesPlace == 7 || onesPlace == 9){
 		let tmp = goal.truncatingRemainder(dividingBy: x);
 		if (tmp == 0 && isPrime(number: x)) {
 			primes = x;
+			break;
 		}
 	}
 }
 
 print(primes);
-
