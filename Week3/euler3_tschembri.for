f5bb058ead273371a34050393798602ea294edae
ebtschembri <41963369+ebtschembri@users.noreply.github.com>
2018-08-21 16:17:59 +0000

    prime factors in Fortran77

---
+++ Week3/euler3_tschembri.for
@@ -0,0 +1,30 @@
+! Fortran77
+! You can test it online at: https://www.onlinegdb.com/online_fortran_compiler (other online compilers don't support comments)
+
+PROGRAM prime_factors
+
+    IMPLICIT NONE;
+    
+    REAL(KIND=8) :: N=600851475143_8                ! KIND=8 and _8 extension: 64bit real 
+    REAL(KIND=8) :: I = 3_8
+    PRINT *, 1                                      ! 1 is always a prime factor
+
+10  IF  ( I .LE. SQRT( N ) ) THEN                   ! every composite number (more than 2 factors) has a prime factor <= to its square root 
+20      IF  ( MODULO( N, I ) .EQ. 0 ) THEN          ! if n can be divided by i then i is a prime number 
+            PRINT *, INT(I)                         ! display the prime number
+            N = N / I                               ! divide n by the prime number
+            GOTO 20                                 ! and loop until n cannot be divided by this prime number
+        END IF                                      
+        I = I + 2                                   ! I = I + 2: skip even numbers, prime numbers are always odd (except 2)
+        GOTO 10                                     ! and restart
+    END IF
+    PRINT *, INT(N)                                 ! once divided by prime factors, n = last prime factor
+
+END PROGRAM prime_factors
+
+! result is:
+!     1
+!    71
+!   839
+!  1471
+!  6857
