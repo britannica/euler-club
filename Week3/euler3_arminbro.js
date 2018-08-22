b68b8823f9ce4c107a79c1f21c77fc5f3ef7855c
Armin Broubakarian <ABroubakarian@eb.com>
2018-08-21 22:26:35 -0500

    Fix indentation

--- Week3/euler3_arminbro.js
+++ Week3/euler3_arminbro.js
@@ -1,12 +1,11 @@
 function getLargestPrimeFactor(number) {
-    let divisor = 2;
-  
-    while (number > 1) {
-      number % divisor === 0 ? number /= divisor : divisor > 2 ? (divisor += 2) : divisor++;
-    }
-  
-    return divisor;
+  let divisor = 2;
+
+  while (number > 1) {
+    number % divisor === 0 ? number /= divisor : divisor > 2 ? (divisor += 2) : divisor++;
   }
+
+  return divisor;
+}
   
-  console.log(getLargestPrimeFactor(600851475143));
-  
+console.log(getLargestPrimeFactor(600851475143));
