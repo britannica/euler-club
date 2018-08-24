ee6bbfd914478f06ada5fed5f87bc4bb8918e5e8
ckissinger <ckissinger@eb.com>
2018-08-24 10:44:54 -0500

    using powers of ten instead of hard coding an array

--- Week4/euler4_ckissinger.rb
+++ Week4/euler4_ckissinger.rb
@@ -3,22 +3,20 @@ def self.num_digits(num)
   num.to_s.size
 end
 
-# place is 1 for ones place, 10 for tens place, etc.
-def self.place(num, place)
-  (num % (place * 10) - num % place) / place
+# power_of_ten is 0 for ones place, 1 for tens place, etc.
+def self.place(num, power_of_ten)
+  (num % (10**(power_of_ten + 1)) - num % 10**power_of_ten) / 10**power_of_ten
 end
 
 def self.palindrome?(num)
-  places = [1, 10, 100, 1000, 10_000, 100_000]
   num_digits = self.num_digits(num)
-  places_slice = places[0, num_digits]
   (0..(num_digits / 2) - 1).each do |index|
     # getting the digit at equal depth from opposite side of number,
     # e.g. hundreds and ones position in 3-digit number,
     # thousands and ones position in 4-digit number,
     # hundreds and tens position in 4-digit number, etc.
-    mirrored_digit_one = place(num, places_slice[index])
-    mirrored_digit_two = place(num, places_slice[-(index + 1)])
+    mirrored_digit_one = place(num, index)
+    mirrored_digit_two = place(num, num_digits - (index + 1))
     return false if mirrored_digit_one != mirrored_digit_two
   end
   true
