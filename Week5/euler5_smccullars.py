05202f1d64ebb3b6ef5d0ea1d3185e9bfd025458
Shawn McCullars <smccullars@eb.com>
2018-08-31 12:22:52 -0500

    good enough to push

--- Week5/euler5_smccullars.py
+++ Week5/euler5_smccullars.py
@@ -1,40 +1,29 @@
-# function borrowed from Week3/euler3_mwiechec.py
-# would have used my own...but alas, i did it in C
-def get_prime_factors(x):
-    z = (x // 2) + 1
-    for p in range(2,z):
-        if (x % p) == 0:
-            x = x // p
-            if x == 1:
-                return [p]
-            else:
-                res = get_prime_factors(x)
-                res.append(p)
-                return res
-    else:
-        return [1,x]
+from math import ceil, sqrt
 
-def factorization_to_hash(h):
-    hash = {}
-    for fac in h:
-        if hash.get(fac):
-            hash[fac] += 1
+# borrowed from https://codereview.stackexchange.com/questions/19509/functional-prime-factor-generator
+def factor(n):
+    if n <= 1: return []
+    prime = next((x for x in range(2, ceil(sqrt(n))+1) if n%x == 0), n)
+    return [prime] + factor(n//prime)
+
+def factorization_to_dict(f):
+    d = {}
+    for fac in f:
+        if d.get(fac):
+            d[fac] += 1
         else:
-            hash[fac] = 1
-    return hash
+            d[fac] = 1
+    return d
+
+
 
 factors = {}
 
 for num in range(2,21):
-    factorization = get_prime_factors(num)
-    for fac, multiple in factorization_to_hash(factorization).items():
-        if factors.get(fac):
-            if factors[fac] < multiple:
-                 factors[fac] = multiple
-        else:
+    factorization = factorization_to_dict(factor(num))
+    for fac, multiple in factorization.items():
+        if not factors.get(fac) or factors[fac] < multiple:
             factors[fac] = multiple
-
-print(factors)
 
 smallest_multiple = 1
 
