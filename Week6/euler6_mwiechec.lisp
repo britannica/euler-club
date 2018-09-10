3baecaa2cb0afe44d92ec8e3e329f28a450cd33d
Mark Wiec hec <mark.wiechec@gmail.com>
2018-09-10 09:59:04 -0500

    added comments

--- Week6/euler6_mwiechec.lisp
+++ Week6/euler6_mwiechec.lisp
@@ -1,13 +1,20 @@
+;;; common lisp solution to Euler Week 6
 (defun square (x) (* x x))
 
+;;; recursive loop from n to 1 summing as you come back
 (defun sumrange (n)
   (if (= n 1)
       1
-    (+ n (sumrange (- n 1)))))
-    
+    (+ n (sumrange (- n 1)))
+  )
+)
+
+;;; recursive loop from n to 1 summing squares    
 (defun sumsqrange (n)
   (if (= n 1)
       1
-    (+ (square n) (sumsqrange (- n 1)))))    
+    (+ (square n) (sumsqrange (- n 1)))
+  )
+)    
 
 (write (- (square(sumrange 100)) (sumsqrange 100)))
