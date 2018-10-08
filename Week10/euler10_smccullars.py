2c649e1569c5465d1605238054401bbfbe5f42f7
Shawn McCullars <smccullars@eb.com>
2018-10-08 11:10:00 -0500

    now 30% faster (thanks to jeff for the tip on using an array of booleans to store the sieve)

--- Week10/euler10_smccullars.py
+++ Week10/euler10_smccullars.py
@@ -1,11 +1,11 @@
 max_number = 2000000
 
-not_primes = set()
+#index is offset by 2..ie. the sieve's position 0 is 2, position 1 is 3, etc.
+sieve = [True for x in range(2,max_number)]
 
 for x in range(2,max_number):
     for not_prime in range(2*x, max_number, x):
-        not_primes.add(not_prime)
+        sieve[not_prime-2] = False
 
-sieve = [x for x in range(2,max_number) if x not in not_primes]
-
-print(sum(sieve))
+primes = [x+2 for x in range(0, len(sieve)) if sieve[x]]
+print(sum(primes))
