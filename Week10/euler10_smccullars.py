b34b852e72744f307cb949bf535b85e1bd25a58d
Shawn McCullars <smccullars@eb.com>
2018-10-10 11:37:49 -0500

    forgot to push

--- Week10/euler10_smccullars.py
+++ Week10/euler10_smccullars.py
@@ -1,9 +1,11 @@
+import math
+
 max_number = 2000000
 
 #index is offset by 2..ie. the sieve's position 0 is 2, position 1 is 3, etc.
 sieve = [True for x in range(2,max_number)]
 
-for x in range(2,max_number):
+for x in range(2,math.floor(math.sqrt(max_number))):
     for not_prime in range(2*x, max_number, x):
         sieve[not_prime-2] = False
 
