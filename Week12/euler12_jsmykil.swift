86d99cfd2c3d5b6f7895740a376eb3857e8e7528
Jeffrey Smykil <jeff.smykil@gmail.com>
2018-10-20 17:56:03 -0500

    Commented out.

--- Week12/euler12_jsmykil.swift
+++ Week12/euler12_jsmykil.swift
@@ -1,5 +1,9 @@
+// [Done] exited with code=0 in 1.367 seconds
+// 🙄 This is annoying but is necessary to do 
+// square root
 import Foundation;
 
+// Returns the nth 🔺 number
 func getTriangularNumber(number:Int)->Int {
     var result = 0
     for i in 1...number {
@@ -8,10 +12,17 @@ func getTriangularNumber(number:Int)->Int {
     return result
 }
 
+// Returns an array of factors up until the ⬜ root
 func getFactors(number:Int)->[Int] {
+    // Finding of the ⬜ root of our number involves 
+    // first turning it into a floaitng point number
     let floatingNumber = Double(number);
+    // Get the ⬜ root of our number and use 🎒 rounding
     let floatingNumberSquareRoot = sqrt(floatingNumber).rounded()
+    // Convert the double back into an int so we can
+    // ➰ to it, otherwise we would have to use stride.
     let goalNumber = Int(floatingNumberSquareRoot);
+    // Storage for all of our factors
     var arrayOfInts: [Int] = []
     for i in 1...goalNumber {
         if (number%i == 0) {
@@ -27,6 +38,8 @@ var i = 1;
 while count < numberOfMinDesiredFactors {
     let nthFactor = getTriangularNumber(number: i);
     let factors = getFactors(number: nthFactor);
+    // Count the number of factors and double it.
+    // Are all triangle numbers square rootable with a whole number?
     count = factors.count * 2;
     i = i + 1;
 }
