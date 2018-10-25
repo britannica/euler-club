a2dbfd464d9155e5eca05f3dd7191f669e5dc010
Shawn McCullars <smccullars@eb.com>
2018-10-25 17:26:45 -0500

    nice and tidy

--- Week13/euler13_smccullars.py
+++ Week13/euler13_smccullars.py
@@ -1,7 +1,7 @@
 sum = 0
 
-with open('content.txt') as f:
-    for fifty_digit_number in f:
+with open('one_hundred_big_numbers.txt') as the_numbers:
+    for fifty_digit_number in the_numbers:
         sum += int(fifty_digit_number.strip())
 
 print(str(sum)[0:10])
