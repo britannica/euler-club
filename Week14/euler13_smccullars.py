5e76164fbae3e6127cfb1f89b9f1d711611f8c3d
Shawn McCullars <smccullars@eb.com>
2018-11-05 18:01:40 -0600

    with comments

--- Week14/euler13_smccullars.py
+++ Week14/euler13_smccullars.py
@@ -1,26 +1,26 @@
-known_sequences = {1:1}
+# initialize the chain starts'n'counts with the identity 1:1
+chain_starts_and_counts = {1:1}
 
-def next(seq):
-    i = seq[-1]
-    if i in known_sequences:
-        if len(seq) > 1:
-            seq.pop()
-            #done
-            for n in seq:
-                known_sequences[n] = len(seq) - seq.index(n) + known_sequences[i]
-#        print(seq)
-        return known_sequences[seq[0]]
-    elif i % 2 == 0:
-        seq.append(int(i/2))
+def make_chain(this_chain):
+    # the last item in the chain is the current link to check
+    link = this_chain[-1]
+    # have we seen this chain start before?
+    if link in chain_starts_and_counts:
+        # we did! process this chain...
+        for start in this_chain:
+            # the count for start is the how many more entries exist between
+            # start's position in this chain and the end of this chain plus
+            # the count that we already calculated
+            chain_starts_and_counts[start] = len(this_chain) - this_chain.index(start) + chain_starts_and_counts[link]
+        return
+    elif link % 2 == 0:
+        this_chain.append(int(link/2))
     else:
-        seq.append(3*i+1)
-    return next(seq)
+        this_chain.append(3*link+1)
+    make_chain(this_chain)
 
-longest_start_number = 0
-longest_chain_length = 0
 for i in range(1,1000001):
-    chain_length = next([i])
-    if chain_length > longest_chain_length:
-        longest_start_number = i
-        longest_chain_length = chain_length
-print(longest_start_number)
+    make_chain([i])
+
+#use the lambda to dynamically fetch the value from each key to pass into max()
+print(max(chain_starts_and_counts, key=lambda k: chain_starts_and_counts[k]))
