fedd72b19b89f06ea36260a0f5d1e4cab4dfb1a7
Shawn McCullars <smccullars@eb.com>
2018-11-20 11:00:15 -0600

    now with a comment

--- Week16/euler16_smccullars.py
+++ Week16/euler16_smccullars.py
@@ -1,1 +1,4 @@
+# calculate the exponent, make it a string, make that a list of characters, 
+# use list comprehension to make it a list of integers, sum that list, 
+# and then print the total
 print(sum([int(x) for x in list(str(2**1000))]))
