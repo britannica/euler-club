01450e236be1fa75aa8db6bd28398652e061c23e
Constantin Pojoga <pojogac@gmail.com>
2019-01-14 14:45:37 -0600

    factorial fn update

--- Week20/euler20_cpojoga.js
+++ Week20/euler20_cpojoga.js
@@ -1,6 +1,10 @@
 // Using JS build in BigNumbers
-const factorial = n => n <= 1n ? 1n : n * factorial(n -1n);
+const factorial = (n) => {
+	const big1 = BigInt(1);
+    const bigN = BigInt(n);
+    return (bigN <= big1) ? big1 : bigN * factorial(bigN - big1);
+}
 const sumOfDigits = str => str.split('').reduce((a, b) => parseInt(a) + parseInt(b));
 
-console.log(sumOfDigits(factorial(100n).toString()));
+console.log(sumOfDigits(factorial(100).toString()));
 // 648
