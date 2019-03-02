ca0b599bb31be5eb37f09d93650db91593a5a051
Shawn McCullars <smccullars@eb.com>
2019-03-01 18:35:57 -0600

    better, but still not best

--- Week26/euler26_smccullars.py
+++ Week26/euler26_smccullars.py
@@ -1,22 +1,31 @@
-def next_quotient(x,y):
-    return (x*10) % y
+# find the remainder, carry down the zero
+def next_step(dividend, divisor):
+    return (dividend % divisor) * 10
 
-def long_division_quotients(x,y):
-    partial_quotients = []
-    while next_quotient(x,y) not in partial_quotients:
-        partial_quotients.append(next_quotient(x,y))
-        x = next_quotient(x,y)
-    if partial_quotients[-1] == 0:
-        return 0
+def long_division(dividend, divisor):
+    steps = [next_step(dividend,divisor)]
+    while next_step(steps[-1], divisor) not in steps:
+        steps.append(next_step(steps[-1], divisor))
+    # if our last step returned zero, there is no cycle
+    # because the long division process ended successfully
+    if steps[-1] == 0:
+        return []
     else:
-        return len(partial_quotients)
+        # the cycle may only be a subset of all steps (ie. 1/6 or 1/76)
+        # ignore everything until we find the beginning step
+        for index,step in enumerate(steps):
+           if step == next_step(steps[-1], divisor):
+               return steps[index:]
 
-d = 0
-longest_cycle_count = 0
-for i in range(1,1000):
-    cycle_count = long_division_quotients(1,i)
-    if cycle_count > longest_cycle_count:
-        longest_cycle_count = cycle_count
-        d = i
+# Even though there *could* be two cycles of equal length...because
+# the problem clearly says there is one unique answer, we can safely
+# flip the usual key/value pair (to make sorting easier).  When
+# there are multiple cycles of equal length, this approach would
+# result in us finding the largest value of i for that cycle count.
 
-print(d)
+cycles = {}
+for i in range(2,1000):
+#    print('{}: {}'.format(i, long_division(1,i)))
+    cycles[len(long_division(1,i))] = i
+
+print(cycles[max(cycles)])
