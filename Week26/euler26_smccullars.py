73f61b096a721c44bd7a200e1227b2ee8691e12b
Shawn McCullars <smccullars@eb.com>
2019-03-04 11:03:46 -0600

    an OO approach (in python)

--- Week26/euler26_smccullars.py
+++ Week26/euler26_smccullars.py
@@ -1,21 +1,30 @@
-# find the remainder, carry down the zero
-def next_step(dividend, divisor):
-    return (dividend % divisor) * 10
+class LongDivisionStepper:
+    def __init__(self, dividend, divisor):
+        self.divisor = divisor
+        self.steps = [dividend]
 
-def long_division(dividend, divisor):
-    steps = [next_step(dividend,divisor)]
-    while next_step(steps[-1], divisor) not in steps:
-        steps.append(next_step(steps[-1], divisor))
-    # if our last step returned zero, there is no cycle
-    # because the long division process ended successfully
-    if steps[-1] == 0:
-        return []
-    else:
-        # the cycle may only be a subset of all steps (ie. 1/6 or 1/76)
-        # ignore everything until we find the beginning step
-        for index,step in enumerate(steps):
-           if step == next_step(steps[-1], divisor):
-               return steps[index:]
+    def step(self):
+        # modulus to find the remainder, carry down the zero
+        return (self.steps[-1] % self.divisor) * 10
+
+    def cycle(self):
+        # as long as the next step is not a repeated step, keep going
+        while self.step() not in self.steps:
+            self.steps.append(self.step())
+
+        # if the last step returned zero, the division successfully
+        # terminated and there is no cycle
+        if self.steps[-1] == 0:
+            return []
+        else:
+            # the cycle may only be a subset of all steps (ie. 1/6 
+            # or 1/76) ignore everything until we find the beginning
+            # step (aka the repeated step)
+
+            repeated_step = self.step()
+            for index,step in enumerate(self.steps):
+                if step == repeated_step:
+                    return self.steps[index:]
 
 # Even though there *could* be two cycles of equal length...because
 # the problem clearly says there is one unique answer, we can safely
@@ -25,7 +34,7 @@ def long_division(dividend, divisor):
 
 cycles = {}
 for i in range(2,1000):
-#    print('{}: {}'.format(i, long_division(1,i)))
-    cycles[len(long_division(1,i))] = i
+    cycle = LongDivisionStepper(1,i).cycle()
+    cycles[len(cycle)] = i
 
 print(cycles[max(cycles)])
