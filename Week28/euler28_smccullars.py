6263de843ff77a6aa17cc414eef8fe079ee1606b
Shawn McCullars <smccullars@eb.com>
2019-03-13 14:31:51 -0500

    clearer logic

--- Week28/euler28_smccullars.py
+++ Week28/euler28_smccullars.py
@@ -1,8 +1,10 @@
+# initialize center point, spiral is 1x1
 diagonal_points = [1]
 
-for spiral_edge_size in range(1, 1001, 2):
-    spiral_edge_size += 2
+# begin with 2x2 spiral, go until 1001x1001 (inclusive), 
+# each spiral increases the edge by 2
+for spiral_edge_size in range(2, 1002, 2):
     for i in range(0,4):
-        diagonal_points.append(diagonal_points[-1] +  spiral_edge_size - 1)
+        diagonal_points.append(diagonal_points[-1] +  spiral_edge_size)
 
 print(sum(diagonal_points))
