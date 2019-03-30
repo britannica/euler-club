4f11433cc9ecd3d86e0a096067520584768180f8
sbosco <sbosco@eb.com>
2019-03-30 14:34:21 -0500

    Create euler30_sbosco.py

---
+++ Week30/euler30_sbosco.py
@@ -0,0 +1,33 @@
+# euler30
+
+import time
+t0 = time.time()
+
+power = 5
+
+def upperlimit(power):
+    digits = 1
+    while(len(str(9**power*digits)) > digits):
+        digits += 1
+    return 9**power*digits
+
+
+maxnum = upperlimit(power)
+answer = 0
+
+for x in range(2, maxnum+1):
+    total = 0
+    digits = list(str(x))
+    for digit in digits:
+        total += int(digit)**power
+    if total == x:
+        print("{0}".format(x))
+        answer += x       
+
+        
+print('sum of terms={0}'.format(answer))
+ 
+
+t1 = time.time()
+elapsed = t1 - t0
+print('time={0}'.format(elapsed))
