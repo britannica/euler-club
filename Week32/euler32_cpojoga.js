183bf19bc4e3a901d0e52abf93f44ac92fffb7de
Constantin Pojoga <pojogac@gmail.com>
2019-04-11 14:07:51 -0500

    euler32, solution 2

--- Week32/euler32_cpojoga.js
+++ Week32/euler32_cpojoga.js
@@ -1,30 +1,17 @@
-// Generate all 9 digits pandigitals:
-const pandigitals = [];
-const comb = (d, u) => {
-  for (let i = 0; i < d.length; i++) {
-    const u1 = [...u, d[i]];
-    
-    if (u1.length === 9) 
-      pandigitals.push(u1.join(''));
-    else
-      comb([...d.slice(0, i), ...d.slice(i + 1)], u1);
+const isPandigitalOf9 = str => str.split('').sort().join('') === '123456789';
+const isPandigitalOf9Prod = (a, b) => isPandigitalOf9('' + a + b + a * b);
+const res = [];
+
+for (let i = 1; i < 9; i++) {
+  for (let j = 1234; j < 9876; j++) {
+    if (res.indexOf(i * j) === -1 && isPandigitalOf9Prod(i, j)) res.push(i * j)
+  }
+}
+for (let i = 12; i < 98; i++) {
+  for (let j = 123; j < 987; j++) {
+    if (res.indexOf(i * j) === -1 && isPandigitalOf9Prod(i, j)) res.push(i * j)
   }
 }
 
-const num = (str, start, end) => parseInt(str.substring(start, end));
-
-comb([1,2,3,4,5,6,7,8,9], []);
-
-const res = [];
-// Find all unique products:
-for (let i = 0; i < pandigitals.length; i++) {
-    const el = pandigitals[i];
-    const prod = num(el, 5, 9);
-
-    if (res.indexOf(prod) === -1 && (num(el, 0, 1) * num(el, 1, 5) === prod || num(el, 0, 2) * num(el, 2, 5) === prod)) {
-        res.push(prod)
-    }
-}
-
-console.log(res.reduce((a, b) => a + b))
-// 45228 (450ms)
+console.log(res.reduce((a, b) => a + b));
+// 45228 (199ms)
