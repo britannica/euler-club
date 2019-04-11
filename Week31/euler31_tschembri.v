5232ed0f16c6e6ce7872edbb9a82c637bd25019f
ebtschembri <41963369+ebtschembri@users.noreply.github.com>
2019-04-10 21:39:27 -0500

    added comments

--- Week31/euler31_tschembri.v
+++ Week31/euler31_tschembri.v
@@ -44,31 +44,31 @@ module main;
     initial 
     begin
        
-        for ( i = 200; i >= 0; i -= 200) 
-		begin
-			for ( j = i; j >= 0; j -= 100)
-			begin
-				for ( k = j; k >= 0; k -= 50)
-				begin
-					for ( l = k; l >= 0; l -= 20)
-					begin
-						for ( m = l; m >= 0; m -= 10) 
-						begin
-							for ( n = m; n >= 0; n -= 5) 
-							begin
-								for ( o = n; o >= 0; o -= 2)
-								begin
-									total++;
-								end
-							end
-						end
-					end
-				end
-			end
-		end
+      for ( i = 200; i >= 0; i -= 200) 
+      begin
+	  for ( j = i; j >= 0; j -= 100)
+	  begin
+	     for ( k = j; k >= 0; k -= 50)
+	     begin
+	         for ( l = k; l >= 0; l -= 20)
+	         begin
+		     for ( m = l; m >= 0; m -= 10) 
+		     begin
+			 for ( n = m; n >= 0; n -= 5) 
+			 begin
+			     for ( o = n; o >= 0; o -= 2)
+			     begin
+				 total++;
+		 	     end
+			 end
+		     end
+	         end
+	     end
+	 end
+     end
 		    
-      $display(total);
-      $finish ;
+     $display(total);
+     $finish ;
     end
 endmodule
 
