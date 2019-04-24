04563577d3b5edddc6324e1d1098bb1dab436ffc
Shawn McCullars <smccullars@eb.com>
2019-04-24 13:17:55 -0500

    correct brute force solution...now i just gotta figure out how/when/why to bound the range and maybe other performance optimizations

--- Week34/euler34_smccullars.py
+++ Week34/euler34_smccullars.py
@@ -3,11 +3,11 @@ from math import factorial
 total = 0
 
 for x in range(3, 100000):
-    target = x
+    factorial_sum = 0
     for n in list(str(x)):
-        if target >= 0:
-            target -= factorial(int(n))
-    if target == 0:
+        if x >= factorial_sum:
+            factorial_sum += factorial(int(n))
+    if factorial_sum == x:
         total += x
 
 print(total)
