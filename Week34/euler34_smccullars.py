1e16279073bc8b0974f188313c444449ce8dd981
Shawn McCullars <smccullars@eb.com>
2019-04-25 10:12:17 -0500

    super fast, but still no good reason for the loop upper bound (other than the solution is correct)

--- Week34/euler34_smccullars.py
+++ Week34/euler34_smccullars.py
@@ -1,13 +1,15 @@
-from math import factorial
+factorials = {}
 
-total = 0
+for x in range(0,10):
+    factorials[x] = x * factorials[x-1] if x-1 in factorials else 1
+
+curious_number_total = 0
 
 for x in range(3, 100000):
     factorial_sum = 0
     for n in list(str(x)):
-        if x >= factorial_sum:
-            factorial_sum += factorial(int(n))
+        factorial_sum += factorials[int(n)]
     if factorial_sum == x:
-        total += x
+        curious_number_total += x
 
-print(total)
+print(curious_number_total)
