f16eb63a71725154016a63aa540f1e3193a3f31a
ebtschembri <41963369+ebtschembri@users.noreply.github.com>
2019-06-03 11:07:56 -0500

    Euler 38 in Java

---
+++ Week38/euler38_tschembri.java
@@ -0,0 +1,37 @@
+/*
+Euler 38 in Java
+We stop at 9999 because concatenating two 5 digit numbers will give a 10 digit number (which can't be pandigital)
+*/
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Euler {
+
+    public static void main(String[] args) {
+
+        Instant start = Instant.now();
+        System.out.println(
+            IntStream.range( 1, 10000 )
+                     .boxed()
+                     .map ( i -> { int n = 1; String m = ""; while ( ( m += ( i * n++) ).length() < 9 ); return m; } )
+                     .filter( m -> Stream.of( m.split( "" ) ).sorted().collect( Collectors.joining() ).equals( "123456789" ) )
+                     .max( Comparator.naturalOrder() ).get()
+        );
+        System.out.println( String.format( "%dms", Duration.between( start, Instant.now() ).toMillis() ) );
+    }
+}
+
+
+// --- output
+
+// Connected to the target VM, address: '127.0.0.1:65092', transport: 'socket'
+// 932718654
+// 127ms
+// Disconnected from the target VM, address: '127.0.0.1:65092', transport: 'socket'
+//
+// Process finished with exit code 0
