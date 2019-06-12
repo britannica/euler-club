605cbdccc1046015cb368cf348c0b41265d60b19
Shawn McCullars <smccullars@eb.com>
2019-06-12 11:38:58 -0500

    slightly tweaked bounding, prepopulating the solutions dict (to remove two lines/else clause)

--- Week39/euler39_smccullars.py
+++ Week39/euler39_smccullars.py
@@ -1,11 +1,11 @@
 from math import sqrt
 
-# assume a ≤ b ≤ c
+# assume a ≤ b < c
 
 # given:
 #   a^2+b^2=c^2
 #   a+b+c=p
-#   a ≤ 1, b ≤ 1, c ≤ 1
+#   a ≥ 1, b ≥ 1, c ≥ 2
 #   a,b,c are integers
 # simplify and solve for b:
 #   a^2+b^2=(p-a-b)^2
@@ -14,18 +14,16 @@ from math import sqrt
 #   p^2-2pa=b(2p-2a)
 #   b=(p^2-2pa)/2(p-a)
 
-solutions = {}
+solutions = {p: 0 for p in range(4, 1001)}
 
-for p in range(3, 1000):
-    for a in range(1, p):
+for p in range(4, 1001):
+    for a in range(1, p-1):
         b = (p**2-(2*p*a))/(2*(p-a))
         if int(b) == b and a <= b:
             c = sqrt(a**2+b**2)
             if a+b+c == p:
                 if p in solutions:
                     solutions[p] += 1
-                else:
-                    solutions[p] =1
 
 max_solution = max(solutions.values())
 print([p for p in solutions if solutions[p] == max_solution])
