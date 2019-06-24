2249bea2cf26aecdfae2bd1d078bdeb42b5194e8
Constantin Pojoga <pojogac@gmail.com>
2019-06-24 17:52:12 -0500

    euler40_cpojoga.js

--- Week40/euler40_cpojoga.js
+++ Week40/euler40_cpojoga.js
@@ -1,8 +1,8 @@
 let fr = '';
 let i = 1;
-while (fr.length < 1e6 ) fr += i++;
+while (fr.length <= 1e6 ) fr += i++;
 
 let prod = 1;
-for (let j = 0; j <= 6; j++) prod *= fr.charAt( 10 ** j - 1);
+for (let j = 0; j <= 6; j++) prod *= fr[10 ** j -1];
 
 console.log(prod);
